fddbd817f88e6715114eb4203e7eb420b44891a2
navinduk <navindu.nk@gmail.com>
2020-07-28 19:42:22 +0530

    Express Routers

--- week-1/3-node-express/index.js
+++ week-1/3-node-express/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 // const fs = require('fs');
 // const path = require('path');
 
@@ -12,46 +13,8 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.all('/dishes',(req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next()
-});
 
-app.get('/dishes',(req,res,next)=>{
-    res.end('Will send all dishes');
-});
-
-app.post('/dishes',(req,res,next)=>{
-    res.end('Will add the dish: '+req.body.name+' With details: '+req.body.description);
-});
-
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('Put not support on dishes');
-});
-
-app.delete('/dishes',(req,res,next)=>{
-    res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('Will send details of the dish: '+req.params.dishId);
-});
-
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('Put not support on dish: '+req.params.dishId);
-});
-
-app.put('/dishes/:dishId',(req,res,next)=>{
-    res.write('Updating the dish: ' +req.params.dishId+'\n')
-    res.end('will update the dish: '+req.body.name+' with details: '+req.body.description);
-});
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end('Deleting dish: '+req.params.dishId);
-});
+app.use('/dishes',dishRouter);
 
 app.use(express.static(__dirname+'/public'));
 
