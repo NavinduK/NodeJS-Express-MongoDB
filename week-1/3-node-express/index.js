1391d7ff124966e0795c081b74634c52606244c9
navinduk <navindu.nk@gmail.com>
2020-07-28 14:17:47 +0530

    Express Static files serve

--- week-1/3-node-express/index.js
+++ week-1/3-node-express/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const morgan = require('morgan');
 
 // const fs = require('fs');
 // const path = require('path');
@@ -8,9 +9,11 @@ const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
 
 app.use((req,res,next)=>{
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html')
     res.end('<html><body><h1>Express server</h1></body></html>');
